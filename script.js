0b9bd7c1c6565c80a8377f2bb9b39d588bd9be99
Munsa1 <munsamibenge65@gmail.com>
2023-01-27 20:37:27 +0200

    Select all DOM tags for manipulation

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
