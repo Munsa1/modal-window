83b560cb97a368cf87c896055b5071c0938e1a37
Munsa1 <munsamibenge65@gmail.com>
2023-01-27 20:38:22 +0200

    Loop through nodelist using for-loop

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
+// console.log(btnsOpenModal);
+// let Arr = [1,2,3,4,5]
+
+for(let i = 0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('clicked', function(){
+        console.log('button clicked');
+    })
+}
