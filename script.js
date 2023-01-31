d7a5b81ec30543fa9ac59fc11117d939ffb4e492
Munsa1 <munsamibenge65@gmail.com>
2023-02-01 01:03:59 +0200

    Refactor and document code, also add close feature to 'overlay'

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 'use strict';
+
+
+/*Grab DOM Elements For MAnipulation*/
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
-// console.log(btnsOpenModal);
-// let Arr = [1,2,3,4,5]
 
-for(let i = 0; i < btnsOpenModal.length; i++){
-    btnsOpenModal[i].addEventListener('clicked', function(){
-        console.log('button clicked');
-    })
+/*Reusable Open Modal Function*/
+const showModal = ()=>{
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
 }
+
+
+/*Reusable Close Modal Function*/
+const closeModal = ()=>{
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+/*Show Modal Loop*/
+for(let i = 0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', showModal)
+}   
+
+
+/*Close Modal and Overlay Event Listeners*/
+    btnCloseModal.addEventListener('click', closeModal)
+
+    overlay.addEventListener('click', closeModal)
